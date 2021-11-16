0384f2b87035caa0e99379c611a55ff71c4881d4
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2021-11-16 10:16:31 +0100

    git init

---
+++ src/main/java/backend/thinthere/ThinThereApplication.java
@@ -0,0 +1,13 @@
+package backend.thinthere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinThereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinThereApplication.class, args);
+	}
+
+}

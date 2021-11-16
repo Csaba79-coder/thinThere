db79da1b52881a49e299a1ef494880cc8227d0d8
sumflo <real61madrid@gmail.com>
2021-11-16 16:44:58 +0100

    OrderService - basic

---
+++ src/main/java/backend/thinthere/service/OrderService.java
@@ -0,0 +1,15 @@
+package backend.thinthere.service;
+
+import backend.thinthere.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+  private OrderRepository orderRepository;
+
+  public OrderService(OrderRepository orderRepository) {
+    this.orderRepository = orderRepository;
+  }
+
+}

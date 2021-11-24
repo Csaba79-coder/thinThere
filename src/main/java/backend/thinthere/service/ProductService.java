13a9ea5cd6c4524be7ac6f71ceb84612a124bea8
soma03 <bartfaisoma@gmail.com>
2021-11-24 19:14:13 +0100

    PairProgramming with Gyongyi, Csaba. ProductController + ProductService

--- src/main/java/backend/thinthere/service/ProductService.java
+++ src/main/java/backend/thinthere/service/ProductService.java
@@ -1,4 +1,23 @@
 package backend.thinthere.service;
 
+import backend.thinthere.model.Product;
+import backend.thinthere.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Optional<Product> findById(Long Id) {
+        return productRepository.findById(Id);
+    }
 }

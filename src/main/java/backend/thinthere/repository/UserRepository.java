201614ab9a34f31768163cc8e9acef04a92669cd
gyongyiovics <gyongyimako@gmail.com>
2021-11-26 19:23:26 +0100

    pair-programming with Flora and Soma - updated UserRepository and UserService

--- src/main/java/backend/thinthere/repository/UserRepository.java
+++ src/main/java/backend/thinthere/repository/UserRepository.java
@@ -1,10 +1,16 @@
 package backend.thinthere.repository;
 
 import backend.thinthere.model.User;
+
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByUsername(String username);
+
+  Optional<User> findByID(Long ID);
+
+  List<User> findAll();
 
 }

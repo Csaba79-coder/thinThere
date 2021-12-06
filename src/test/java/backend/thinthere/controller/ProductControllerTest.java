2de3f9a7aefa27ba7f4f8f96c206839ca01dbc2d
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2021-12-06 22:07:29 +0100

    update test and add h2 to pom.xml and create app.prop. to test folder

--- src/test/java/backend/thinthere/controller/ProductControllerTest.java
+++ src/test/java/backend/thinthere/controller/ProductControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = SecurityConfigTest.class)
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 class ProductControllerTest {
 
